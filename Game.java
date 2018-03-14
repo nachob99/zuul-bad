92116d603e89487109fe061d593fb554343eaf1d
nachob99 <nachoarriola99@gmail.com>
2018-03-14 12:01:17 +0100

    añadido metodo eat

--- Game.java
+++ Game.java
@@ -112,6 +112,9 @@ public class Game
         else if (commandWord.equals("look")) {
             look();
         }
+         else if (commandWord.equals("eat")) {
+            eat();
+        }
 
         else if (commandWord.equals("quit")) {
             wantToQuit = quit(command);
@@ -187,5 +190,9 @@ public class Game
     private void look() 
     {
         System.out.println(currentRoom.getLongDescription());
+    }
+    
+    private void eat(){
+        System.out.println("You have eaten now and you are not hungry any more");
     }
 }
